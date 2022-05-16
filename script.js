d86d4a2231d6f7e764dd303fca90fc349bb7dad4
bigmosi <kinyeramo@gmail.com>
2022-05-16 14:56:13 +0300

    fixed indentation in in javascript file

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
+  document.getElementById("myNav").style.width = '100%';
 }
 
+openNav();
+
 function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
+  document.getElementById("myNav").style.width = '0%';
 }
+
+closeNav();
